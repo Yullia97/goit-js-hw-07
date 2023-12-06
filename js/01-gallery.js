2d4d52b624505214c705f1eaed309cf7c0d543b8
Yullia97 <146541652+Yullia97@users.noreply.github.com>
2023-12-06 10:32:07 +0200

    copy

---
+++ js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryList = document.querySelector(".galerry");
+
+const markup = galleryItems
+    .map(
+        (item) => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+
+galleryList.innerHTML = markup;
+
+galleryList.addEventListener("click", onImageItemClick)
+function onImageItemClick(event) {
+    event.preventDefault();
+    const currentItem = event.target;
+    if (currentItem.nodeName !== "IMG") return;
+   
+    const img = currentItem.dataset.source;
+   const instance = basicLightbox.create(`
+   <img src="${img}" alt="${currentItem.alt}">
+`)
+
+instance.show()
+};
+
+console.log(galleryItems);
