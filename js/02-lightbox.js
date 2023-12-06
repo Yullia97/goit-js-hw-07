97ce60a2f4f45e557388bdb09411c8b673fbaf2f
Yullia97 <146541652+Yullia97@users.noreply.github.com>
2023-12-06 12:03:03 +0200

    copy

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,8 +5,10 @@ import { galleryItems } from './gallery-items.js';
 
     const markup = galleryItems
         .map(item => ` <li class="gallery__item">
-            <a class="gallery__link" href="${item.original}">
-                <img class="gallery__image" src="${item.preview}" 
+            <a class="gallery__link"
+            href="${item.original}">
+                <img class="gallery__image"
+                src="${item.preview}" 
                 alt="${item.description}" />
             </a>
         </li>`
